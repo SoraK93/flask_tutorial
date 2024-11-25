f56939dd6bd9d6ac74a592be73b5b4778fed5391
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2024-11-25 12:02:15 +0530

    Database with Flask-SQLAlchemy

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,62 @@
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, ForeignKey, Integer, DateTime, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from form import RegistrationForm, LoginForm
 
-app = Flask(__name__)
 load_dotenv()
 
-app.config["SECRET_KEY"] = "3b1897ae82f207e25bab6779c4fcc234"
-# app.config["SECRET_KEY"] = os.environ("FLASK_KEY")
+
+class Base(DeclarativeBase):
+	pass
+
+
+db = SQLAlchemy(model_class=Base)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("FLASK_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db.init_app(app)
+
+
+class User(db.Model):
+	__tablename__ = "user"
+	id: Mapped[int] = mapped_column(Integer, primary_key=True)
+	username: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+	email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+	image_file: Mapped[str] = mapped_column(String(20), nullable=False, default='default.jpg')
+	password: Mapped[str] = mapped_column(String(60), nullable=False)
+	# This will link the user to their posts
+	posts = relationship('Post', back_populates="author")
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+	__tablename__ = "post"
+	id: Mapped[int] = mapped_column(Integer, primary_key=True)
+	title: Mapped[str] = mapped_column(String(100), nullable=False)
+	date_posted: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now)
+	content: Mapped[str] = mapped_column(Text, nullable=False)
+	author_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'), nullable=False)
+	# This will link the post to the user that wrote it.
+	author = relationship("User", back_populates="posts")
+
+	def __repr__(self):
+		return f"Post('{self.title}','{self.date_posted}')"
+
+
+# with app.app_context():
+	# db.create_all()
+
+	# x = db.session.execute(db.select(User).where(User.id == 1)).scalar()
+
+	# y = db.session.execute(db.select(Post).where(author_id==x.id)).scalar()
+
+	# z = db.session.execute(db.select(Post).options(joinedload(Post.author))).scalar()
 
 
 posts = [
